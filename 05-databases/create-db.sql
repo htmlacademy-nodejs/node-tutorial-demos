4686c1c702bb48ac7e589328054b4273a9d545f9
Igor Antonov <antonov.igor.khv@gmail.com>
2020-06-09 00:58:55 +1000

    Добавляет демонстрации для 5-й главы

---
+++ 05-databases/create-db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE big_library
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+		TEMPLATE template0
+    CONNECTION LIMIT = -1;
