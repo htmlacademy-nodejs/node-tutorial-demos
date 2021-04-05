d0b4d9cc932ada7819033686881a6f71950d7922
Igor Antonov <antonov.igor.khv@gmail.com>
2021-04-05 23:52:25 +0300

    Удалит примеры `request`

--- 03-express/11-request/11-get-with-request.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const request = require(`request-promise-native`);
-const url = `https://jsonplaceholder.typicode.com/posts`;
-
-request(url, {json: true})
-  .then((content) => console.log(content));
